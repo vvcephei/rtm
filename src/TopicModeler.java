494ffb1ebdc7ffa61c8d684210c59fc932e392f2
jroesler <jroesler@jroesler-ub.(none)>
2011-08-01 21:55:45 -0500

    in the midst of adding topic inference for new files

--- src/TopicModeler.java
+++ src/TopicModeler.java
@@ -8,28 +8,22 @@
 
 import Types.Topic;
 import Types.WeightedWord;
-import cc.mallet.util.*;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
-import java.io.Writer;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Formatter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.TreeSet;
 import java.util.regex.Pattern;
@@ -38,9 +32,6 @@ import cc.mallet.types.*;
 import cc.mallet.pipe.*;
 import cc.mallet.pipe.iterator.*;
 import cc.mallet.topics.*;
-
-import com.ibm.wordcloud.Main;
-import org.apache.commons.collections.keyvalue.MultiKey;
 
 
 public class TopicModeler {
@@ -51,7 +42,7 @@ public class TopicModeler {
     private static int _numTopics = 10;
     private static int _numThreads = 10;
     private static int _numTokens = Integer.MAX_VALUE;
-    private static int _numIterations = 500;
+    private static int _numIterations = 100;
     private static double _alphaSum = 100;
     private static double _beta = 0.01;
 
@@ -83,8 +74,8 @@ public class TopicModeler {
 //                model = (TopicModel) new PAM4L(_numTopics / 2, _numTopics, _alphaSum, _beta);
 //                model.estimate(_instances, _numIterations, 50, 50, 0, null, new Randoms());
 //                model.estimate();
-                throw new RuntimeException();
 //                break;
+                throw new RuntimeException();
             default: //LDA
                 model = new ParallelTopicModel();
                 Map params = new HashMap();
@@ -98,7 +89,15 @@ public class TopicModeler {
                 model.estimate();
                 break;
         }
-
+        ParallelTopicModel mymodel = (ParallelTopicModel) model;
+        for (int i = 0; i< 10; i++){
+            System.out.println(Arrays.toString(mymodel.getTopicProbabilities(i)));
+        }
+        System.out.println("Total:");
+        System.out.println(Arrays.toString(mymodel.getTopicProbabilities()));
+        InstanceList test1 = readDir("/data/reviews/bb_eq/test/", _instances.getPipe());
+        
+        System.exit(0);
         return model;
     }
 
@@ -127,19 +126,16 @@ public class TopicModeler {
         return itTopics;
     }
 
-    private void setInstances() {
-        List pipeList = new ArrayList();
-        pipeList.add(new CharSequenceLowercase());
-        pipeList.add(new CharSequence2TokenSequence());
-        pipeList.add(new TokenSequenceRemoveStopwords());
-        pipeList.add(new TokenSequence2FeatureSequence());
+    public InstanceList readDir(String dirName, Pipe pipe){
+        InstanceList instanceList = new InstanceList(pipe);
 
-
-        _instances = new InstanceList(new SerialPipes(pipeList));
-
+        boolean removeCommonPrefix = true;
+        instanceList.addThruPipe(new FileIterator(new File[] {new File(dirName)},
+                FileIterator.STARTING_DIRECTORIES, removeCommonPrefix));
+        return instanceList;
     }
 
-    public void readDir(String dirName) {
+    public InstanceList readDir(String dirName) {
         ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
         pipeList.add(new Target2Label());
         pipeList.add(new SaveDataInSource());
@@ -148,39 +144,27 @@ public class TopicModeler {
         pipeList.add(new CharSequence2TokenSequence());
         pipeList.add(new TokenSequenceRemoveStopwords());
         pipeList.add(new TokenSequence2FeatureSequence());
-/*
-
-
-        Pattern tokenPattern = null;
-        tokenPattern = CharSequenceLexer.LEX_NONWHITESPACE_CLASSES;
-        pipeList.add(new CharSequence2TokenSequence(tokenPattern));
-        pipeList.add(new TokenSequenceLowercase());
-
-        TokenSequenceRemoveStopwords stopwordFilter =
-            new TokenSequenceRemoveStopwords(false, false);
-        pipeList.add(stopwordFilter);
-        pipeList.add( new TokenSequenceNGrams(new int[]{1}) );
-        pipeList.add( new TokenSequence2FeatureSequence() );
-*/
-//        pipeList.add( new FeatureSequence2AugmentableFeatureVector(false) );
-        _instances = new InstanceList(new SerialPipes(pipeList));
-
-
-        boolean removeCommonPrefix = true;
-        _instances.addThruPipe(new FileIterator(new File[] {new File(dirName)},
-                FileIterator.STARTING_DIRECTORIES, removeCommonPrefix));
+        return readDir(dirName, new SerialPipes(pipeList));
     }
 
-    public void readSingleFile(String fileName)
+    public InstanceList readSingleFile(String fileName)
             throws FileNotFoundException, UnsupportedEncodingException {
-        setInstances();
+        List pipeList = new ArrayList();
+        pipeList.add(new CharSequenceLowercase());
+        pipeList.add(new CharSequence2TokenSequence());
+        pipeList.add(new TokenSequenceRemoveStopwords());
+        pipeList.add(new TokenSequence2FeatureSequence());
+
+        InstanceList instanceList = new InstanceList(new SerialPipes(pipeList));
+
         Reader fileReader = new InputStreamReader(new FileInputStream(fileName), "UTF-8");
         Pattern lineRegex = Pattern.compile("^(\\S*)[\\s,]*(\\S*)[\\s,]*(.*)$");
-        _instances.addThruPipe(new CsvIterator(fileReader, lineRegex, 3, 2, 1));
+        instanceList.addThruPipe(new CsvIterator(fileReader, lineRegex, 3, 2, 1));
+        return instanceList;
     }
 
     public static void main(String[] args)
-            throws IOException, UnsupportedEncodingException, InterruptedException {
+            throws IOException, InterruptedException {
         int exitCode = 0;
         TopicModeler tm = new TopicModeler(); //TODO Parse args
         Wordleizer wr = new Wordleizer();
@@ -188,14 +172,14 @@ public class TopicModeler {
 
         File sourcePath = new File(_sourceTextPath);
         if (sourcePath.isDirectory()) {
-            tm.readDir(_sourceTextPath);
+            tm._instances = tm.readDir(_sourceTextPath);
         } else {
-            tm.readSingleFile(_sourceTextPath);
+            tm._instances = tm.readSingleFile(_sourceTextPath);
         }
 
         TopicModel topics = tm.modelTopics(1);//LDA
         exitCode = wr.makeWordles(tm.getTopics(topics));
-
+    
         PrintStream docSummary;
         String separator;
         if (_outputDocSummaryToFile) {
@@ -213,6 +197,8 @@ public class TopicModeler {
         if (_outputDocSummaryToFile){
             docSummary.close();
         }
+
+
         System.exit(exitCode);
     }
 
